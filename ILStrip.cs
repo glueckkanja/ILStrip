cf2eab7a49c32ceb8c29f57ffd328f87ed098733
Felix Storm <felix.storm@glueckkanja.com>
2013-02-14 15:33:18 +0100

    added support for removing resources

---
+++ ILStrip.cs
@@ -0,0 +1,152 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ILStrip
+{
+    class ILStrip
+    {
+        public string InputFileName { get; set; }
+        public string OutputFileName { get; set; }
+        public int Verbose { get; set; }
+
+        public string KeepResources { get; set; }
+        public string RemoveResources { get; set; }
+
+        List<string> _typeIdsFound = new List<string>();
+        int _addScanTypeRecursionLevel = -1;
+        Regex _typeIdRootRegEx = new Regex(@".*?`\d+", RegexOptions.Compiled);
+
+        public void Run()
+        {
+            Console.WriteLine("Opening assembly {0}", InputFileName);
+
+            var assembly = AssemblyDefinition.ReadAssembly(InputFileName);
+            var mainModule = assembly.MainModule;
+            var allTypes = mainModule.Types;
+            var allResources = mainModule.Resources;
+
+
+            _typeIdsFound.Add("<Module>");
+            foreach (var typeDef in allTypes.Where(t => t.IsPublic))
+                AddScanType(typeDef);
+
+            Console.WriteLine("Found {0} accessible types.", _typeIdsFound.Count);
+
+
+            var removeTypeCount = 0;
+            var typeIdRoots = _typeIdsFound.Select(t => GetTypeIdRoot(t)).Distinct().ToList();   // reduce names of generic types including parameters to root type name
+
+            foreach (var typeDef in allTypes.Where(t => !typeIdRoots.Contains(t.FullName)).ToList())
+            {
+                if (Verbose >= 2) Console.WriteLine("Removing: {0}", typeDef);
+                allTypes.Remove(typeDef);
+                removeTypeCount++;
+            }
+
+            Console.WriteLine("Removed {0} inaccessible types.", removeTypeCount);
+
+
+            IEnumerable<Resource> removeResources = null;
+            
+            if (!string.IsNullOrEmpty(KeepResources))
+            {
+                var keep = new List<Resource>();
+                foreach (var resRegEx in KeepResources.Split(','))
+                {
+                    var regEx = new Regex(resRegEx, RegexOptions.Compiled);
+                    keep = keep.Concat(mainModule.Resources.Where(r => regEx.IsMatch(r.Name))).ToList();
+                }
+                removeResources = allResources.Where(r => !keep.Contains(r));
+            }
+            
+            else if (!string.IsNullOrEmpty(RemoveResources))
+            {
+                foreach (var resRegEx in RemoveResources.Split(','))
+                {
+                    var regEx = new Regex(resRegEx, RegexOptions.Compiled);
+                    removeResources = (removeResources ?? new List<Resource>()).Concat(allResources.Where(r => regEx.IsMatch(r.Name)));
+                }
+            }
+
+            if (removeResources != null)
+            {
+                var removeResCount = 0;
+                
+                foreach (var removeRes in removeResources.ToList())
+                {
+                    if (Verbose >= 2) Console.WriteLine("Removing: {0}", removeRes);
+                    allResources.Remove(removeRes);
+                    removeResCount++;
+                }
+                
+                Console.WriteLine("Removed {0} resources.", removeResCount);
+            }
+
+
+            Console.WriteLine("Saving assembly to {0}", OutputFileName);
+            assembly.Write(OutputFileName);
+
+            Console.WriteLine("Done.");
+
+        }
+
+        void AddScanType(TypeReference typeRef)
+        {
+
+            if (typeRef == null || _typeIdsFound.Contains(typeRef.FullName))
+                return;
+
+
+            _addScanTypeRecursionLevel++;
+            if (Verbose >= 1) Console.WriteLine(string.Format("Processing: {0}{1}", "".PadRight(_addScanTypeRecursionLevel * 2), typeRef.FullName));
+
+            _typeIdsFound.Add(typeRef.FullName);
+
+
+            if (typeRef is GenericInstanceType)
+            {
+                foreach (var genericArg in ((GenericInstanceType)typeRef).GenericArguments)
+                    AddScanType(genericArg);
+            }
+
+
+            TypeDefinition typeDef;
+            if ((typeDef = typeRef as TypeDefinition) != null)
+            {
+                var methods = typeDef.Methods.ToList();
+                var bodies = methods.Where(m => m.HasBody).Select(m => m.Body).ToList();
+                var operands = bodies.SelectMany(b => b.Instructions).Select(i => i.Operand).ToList();
+
+                var types = typeDef.Interfaces
+                    .Union(typeDef.CustomAttributes.Select(ca => ca.AttributeType))
+                    .Union(typeDef.Events.Select(e => e.EventType))
+                    .Union(typeDef.Fields.Select(f => f.FieldType))
+                    .Union(typeDef.GenericParameters)
+                    .Union(methods.SelectMany(m => m.Parameters).Select(p => p.ParameterType))
+                    .Union(methods.Select(m => m.ReturnType))
+                    .Union(bodies.SelectMany(b => b.Variables).Select(v => v.VariableType))
+                    .Union(operands.Where(o => o is TypeReference).Select(o => (TypeReference)o))
+                    .Union(operands.Where(o => o is MemberReference).Select(o => ((MemberReference)o).DeclaringType));
+
+                foreach (var t in types)
+                    AddScanType(t);
+            }
+
+
+            _addScanTypeRecursionLevel--;
+        }
+
+        string GetTypeIdRoot(string input)
+        {
+            var match = _typeIdRootRegEx.Match(input);
+            if (match.Success)
+                return match.Value;
+            else
+                return input;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,76 +1,49 @@
-﻿using Mono.Cecil;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ILStrip
 {
     class Program
     {
-        static List<string> _typeIdsFound = new List<string>();
-
         static void Main(string[] args)
         {
-            var assembly = AssemblyDefinition.ReadAssembly(@"C:\Data\GK\Dev\_Temp\ilstrip\GkdcAwsToolsMerged.dll");
-            var allTypes = assembly.MainModule.Types;
+            Console.WriteLine("ILStrip - Copyright Glück & Kanja Consulting AG 2013, see https://github.com/glueckkanja/ILStrip\r\n");
 
-            _typeIdsFound.Add(".<Module>");
-            foreach (var typeDef in allTypes.Where(t => t.IsPublic))
-                AddScanType(typeDef);
 
-            foreach (var typeDef in allTypes.Where(t => !_typeIdsFound.Contains(GetTypeId(t))).ToList())
+            var ilStrip = new ILStrip();
+
+            foreach (var arg in args)
             {
-                //Console.WriteLine("Removing: {0}", typeDef);
-                allTypes.Remove(typeDef);
+                var match = Regex.Match(arg, @"^(?:/|-)(\w+)(?::?)(.*)$");
+                if (match.Success)
+                {
+                    var paramName = match.Groups[1].Value.ToLower();
+                    if (paramName == "out")
+                        ilStrip.OutputFileName = match.Groups[2].Value;
+                    else if (paramName == "keepresources")
+                        ilStrip.KeepResources = match.Groups[2].Value;
+                    else if (paramName == "removeresources")
+                        ilStrip.RemoveResources = match.Groups[2].Value;
+                    else if (paramName == "v")
+                        ilStrip.Verbose++;
+                }
+                else
+                {
+                    ilStrip.InputFileName = arg;
+                }
             }
+            if (string.IsNullOrEmpty(ilStrip.OutputFileName)) ilStrip.OutputFileName = ilStrip.InputFileName;
 
-            assembly.Write(@"C:\Data\GK\Dev\_Temp\ilstrip\GkdcAwsToolsMergedNew.dll");
-        }
-        
-        
-        static int _addScanTypeRecursionLevel = -1;
-
-        static void AddScanType(TypeReference typeRef)
-        {
-            if (typeRef == null || _typeIdsFound.Contains(GetTypeId(typeRef)))
+            if (string.IsNullOrEmpty(ilStrip.InputFileName))
+            {
+                Console.Error.WriteLine("Usage: ilstrip inputfilename /out:outputfilename /keepresources:regex[,regex] /removeresources:regex[,regex]");
                 return;
-
-            _addScanTypeRecursionLevel++;
-            Console.WriteLine(string.Format("Processing: {0}{1}", "".PadRight(_addScanTypeRecursionLevel * 2), typeRef.FullName));
-
-            _typeIdsFound.Add(GetTypeId(typeRef));
-
-            TypeDefinition typeDef;
-            if ((typeDef = typeRef as TypeDefinition) != null)
-            {
-                var methods = typeDef.Methods.ToList();
-                var bodies = methods.Where(m => m.HasBody).Select(m => m.Body).ToList();
-                var operands = bodies.SelectMany(b => b.Instructions).Select(i => i.Operand).ToList();
-
-                var types = typeDef.Interfaces
-                    .Union(typeDef.CustomAttributes.Select(ca => ca.AttributeType))
-                    .Union(typeDef.Events.Select(e => e.EventType))
-                    .Union(typeDef.Fields.Select(f => f.FieldType))
-                    .Union(typeDef.GenericParameters)
-                    .Union(methods.SelectMany(m => m.Parameters).Select(p => p.ParameterType))
-                    .Union(methods.Select(m => m.ReturnType))
-                    .Union(bodies.SelectMany(b => b.Variables).Select(v => v.VariableType))
-                    .Union(operands.Where(o => o is TypeReference).Select(o => (TypeReference)o))
-                    .Union(operands.Where(o => o is MemberReference).Select(o => ((MemberReference)o).DeclaringType));
-                
-                types = types.Union(types.Select(f => f as GenericInstanceType).Where(g => g != null).SelectMany(g => g.GenericArguments));
-
-                foreach (var t in types)
-                    AddScanType(t);
             }
 
-            _addScanTypeRecursionLevel--;
-        }
-
-        static string GetTypeId(TypeReference typeRef)
-        {
-            return string.Format("{0}.{1}", typeRef.Namespace, typeRef.Name);
+            ilStrip.Run();
         }
     }
 }
