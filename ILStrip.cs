ffe06a1945ab8e46c61ded3001b216e7dd03c47a
Jan Berdel <Jan.Berdel@glueckkanja.com>
2014-02-13 13:19:01 +0100

    better scanning of attributes, properties and generic types

--- ILStrip.cs
+++ ILStrip.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-using Mono.Cecil;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Mono.Cecil;
 
 namespace GK
 {
@@ -25,9 +24,9 @@ namespace GK
 
         public int Verbose { get; set; }
 
-        List<string> _typeIdsFound = new List<string>();
+        HashSet<string> _typeIdsFound = new HashSet<string>();
         int _addScanTypeRecursionLevel = -1;
-        Regex _typeIdRootRegEx = new Regex(@".*?`\d+", RegexOptions.Compiled);
+        Regex _typeIdRootRegEx = new Regex(@"((.+)`\d+)", RegexOptions.Compiled);
 
 
         public override bool Execute()
@@ -71,9 +70,8 @@ namespace GK
 
 
             var removeTypeCount = 0;
-            var typeIdRoots = _typeIdsFound.Select(t => GetTypeIdRoot(t)).Distinct().ToList();   // reduce names of generic types including parameters to root type name
-
-            foreach (var typeDef in allTypes.Where(t => !typeIdRoots.Contains(t.FullName)).ToList())
+            
+            foreach (var typeDef in allTypes.Where(t => SafeToRemove(t) && !_typeIdsFound.Contains(t.FullName)).ToList())
             {
                 if (Verbose >= 2) LogLine("Removing: {0}", typeDef);
                 allTypes.Remove(typeDef);
@@ -146,6 +144,15 @@ namespace GK
             return true;
         }
 
+        private bool SafeToRemove(TypeDefinition typeDef)
+        {
+            // never remove attributes
+            if (InheritsFrom(typeDef, typeof(Attribute).FullName))
+                return false;
+
+            return true;
+        }
+
         void AddScanType(TypeReference typeRef)
         {
 
@@ -161,6 +168,9 @@ namespace GK
 
             if (typeRef is GenericInstanceType)
             {
+                _typeIdsFound.Add(GetTypeIdRoot(typeRef.FullName));
+                _typeIdsFound.Add(GetTypeIdRoot(typeRef.FullName, true));
+
                 foreach (var genericArg in ((GenericInstanceType)typeRef).GenericArguments)
                     AddScanType(genericArg);
             }
@@ -169,20 +179,37 @@ namespace GK
             TypeDefinition typeDef;
             if ((typeDef = typeRef as TypeDefinition) != null)
             {
+                if (typeDef.BaseType != null)
+                {
+                    AddScanType(typeDef.BaseType);
+                }
+
                 var methods = typeDef.Methods.ToList();
                 var bodies = methods.Where(m => m.HasBody).Select(m => m.Body).ToList();
                 var operands = bodies.SelectMany(b => b.Instructions).Select(i => i.Operand).ToList();
 
+                methods = methods
+                    .Union(typeDef.Properties.Where(p => p.GetMethod != null).Select(p => p.GetMethod))
+                    .Union(typeDef.Properties.Where(p => p.SetMethod != null).Select(p => p.SetMethod))
+                    .ToList();
+
                 var types = typeDef.Interfaces
                     .Union(typeDef.CustomAttributes.Select(ca => ca.AttributeType))
+                    .Union(typeDef.Events.SelectMany(e => e.CustomAttributes.Select(ca => ca.AttributeType)))
                     .Union(typeDef.Events.Select(e => e.EventType))
+                    .Union(typeDef.Fields.SelectMany(f => f.CustomAttributes.Select(ca => ca.AttributeType)))
                     .Union(typeDef.Fields.Select(f => f.FieldType))
+                    .Union(typeDef.Properties.SelectMany(p => p.CustomAttributes.Select(ca => ca.AttributeType)))
                     .Union(typeDef.GenericParameters)
+                    .Union(methods.SelectMany(m => m.Parameters).SelectMany(p => p.CustomAttributes.Select(ca => ca.AttributeType)))
+                    .Union(methods.SelectMany(m => m.CustomAttributes).Select(ca => ca.AttributeType))
                     .Union(methods.SelectMany(m => m.Parameters).Select(p => p.ParameterType))
                     .Union(methods.Select(m => m.ReturnType))
+                    .Union(methods.Select(m => m.MethodReturnType.ReturnType))
+                    .Union(methods.SelectMany(m => m.MethodReturnType.CustomAttributes.Select(ca => ca.AttributeType)))
                     .Union(bodies.SelectMany(b => b.Variables).Select(v => v.VariableType))
-                    .Union(operands.Where(o => o is TypeReference).Select(o => (TypeReference)o))
-                    .Union(operands.Where(o => o is MemberReference).Select(o => ((MemberReference)o).DeclaringType));
+                    .Union(operands.OfType<TypeReference>())
+                    .Union(operands.OfType<MemberReference>().Select(x => x.DeclaringType));
 
                 foreach (var t in types)
                     AddScanType(t);
@@ -192,13 +219,32 @@ namespace GK
             _addScanTypeRecursionLevel--;
         }
 
-        string GetTypeIdRoot(string input)
+        private string GetTypeIdRoot(string input, bool stripCount = false)
         {
-            var match = _typeIdRootRegEx.Match(input);
+            Match match = _typeIdRootRegEx.Match(input);
+
             if (match.Success)
-                return match.Value;
-            else
-                return input;
+                return stripCount ? match.Groups[1].Value : match.Groups[2].Value;
+
+            return input;
+        }
+
+        private static bool InheritsFrom(TypeDefinition typeDef, string fullName)
+        {
+            TypeDefinition type = typeDef;
+
+            if (type.FullName == fullName)
+                return true;
+
+            while (type.BaseType != null)
+            {
+                type = type.BaseType.Resolve();
+
+                if (type.FullName == fullName)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
